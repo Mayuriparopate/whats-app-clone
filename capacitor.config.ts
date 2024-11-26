e45e507683f0d6224b57e5d1289651f1a98f8557
Mayuriparopate <mayuriparopate7@gmail.com>
2024-11-26 17:03:55 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whats-app-clone',
+  webDir: 'dist'
+};
+
+export default config;
